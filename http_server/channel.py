ec399ead2b46475546e21ab8568708ad84d452a0
jackcizon <jack20021213cn@gmail.com>
2026-03-05 17:55:11 +0800

    refactor: channel

--- http_server/channel.py
+++ http_server/channel.py
@@ -1,41 +1,36 @@
-from enum import Enum
+from socket import socket
 from typing import Any
 from collections.abc import Callable
 
-
-class FdEvent(Enum):
-    TIMEOUT = 0b001
-    READ_EVENT = 0b010
-    WRITE_EVENT = 0b100
+from http_server import constants
 
 
 class Channel:
     def __init__(
             self,
-            fd: int,
+            sock: socket,
+            # fd: int,
             events: int,
             read_callback: Callable,
             write_callback: Callable,
             destroy_callback: Callable,
             args: Any
     ):
-        self._fd = fd
-        self._events = events
-        self._read_callback = read_callback
-        self._write_callback = write_callback
-        self._destroy_callback = destroy_callback
-        self._args = args
+        self.sock = sock
+        # self.fd = fd
+        self.fd = sock.fileno()
+        self.events = events
+        self.read_callback = read_callback
+        self.write_callback = write_callback
+        self.destroy_callback = destroy_callback
+        self.args = args
 
     def is_writeable(self):
-        return self._events & FdEvent.WRITE_EVENT.value
+        return self.events & constants.CHANNEL_WRITE_EVENT
 
     def writable(self, flag: bool):
         if flag is True:
-            self._events |= FdEvent.WRITE_EVENT.value
+            self.events |= constants.CHANNEL_WRITE_EVENT
         else:
-            not_writable = ~FdEvent.WRITE_EVENT.value
-            self._events &= not_writable
-
-
-if __name__ == '__main__':
-    print(0b001 & 0b101)
+            not_writable = ~constants.CHANNEL_WRITE_EVENT
+            self.events &= not_writable
