f31d592e1bb5cafbbf4ed39b269174c351129be4
jackcizon <jack20021213cn@gmail.com>
2026-03-05 23:53:11 +0800

    refactor(channel): add some properties, fix issues.

--- http_server/channel.py
+++ http_server/channel.py
@@ -11,26 +11,41 @@ class Channel:
             sock: socket,
             # fd: int,
             events: int,
-            read_callback: Callable,
-            write_callback: Callable,
-            destroy_callback: Callable,
+            read_callback: Callable | None,
+            write_callback: Callable | None,
+            destroy_callback: Callable | None,
             args: Any
     ):
         self.sock = sock
         # self.fd = fd
-        self.fd = sock.fileno()
-        self.events = events
+        self._fd = sock.fileno()
+        self._events = events
         self.read_callback = read_callback
         self.write_callback = write_callback
         self.destroy_callback = destroy_callback
-        self.args = args
+        self._args = args
 
     def is_writeable(self):
         return self.events & constants.CHANNEL_WRITE_EVENT
 
     def writable(self, flag: bool):
         if flag is True:
-            self.events |= constants.CHANNEL_WRITE_EVENT
+            self._events |= constants.CHANNEL_WRITE_EVENT
         else:
             not_writable = ~constants.CHANNEL_WRITE_EVENT
-            self.events &= not_writable
+            self._events &= not_writable
+
+    @property
+    def fd(self):
+        return self._fd
+
+    @property
+    def events(self):
+        return self._events
+
+    def set_event(self, event_mask: int):
+        self._events = event_mask
+
+    @property
+    def args(self):
+        return self._args
