5b57f257d9816f2c507b97347d8e0f443a600e51
jackcizon <jack20021213cn@gmail.com>
2026-03-06 00:03:31 +0800

    refactor(epoll_dispatcher):  - drop some unnecessary methods' params.  - update init()  - clean the cls.

--- http_server/dispatcher/epoll_dispatcher.py
+++ http_server/dispatcher/epoll_dispatcher.py
@@ -1,71 +1,76 @@
-from os import close
-from typing import Any
+import os
 from select import EPOLLIN, EPOLLOUT, EPOLLHUP, EPOLLERR, epoll
 
 from http_server.channel import Channel
 from http_server import constants
-from http_server.dispatcher.base_dispatcher import AbstractDispatcher
+from http_server.dispatcher.base_dispatcher import DispatcherInterface
+from http_server.eventloop import EventLoop
 
 
-class EpollData:
-    def __init__(self, max_events: int = 1024):
+class EpollDispatcher(DispatcherInterface):
+    """a handler that resolve dispatch problems"""
+
+    def __init__(
+            self,
+            loop: EventLoop,
+            *,
+            name: str = "epoll",
+            timeout: float = 2.0,
+            max_events: int = 1024
+    ):
+        self._loop = loop
+        self._name = name
+        self._timeout = timeout
+        self._channel: Channel | None = None
         self.ep = epoll()
         self.events: list[tuple[int, int]] = []
         self.efd = self.ep.fileno()
         self.max_events = max_events
 
+    def clear(self):
+        os.close(self.efd)
+        del self
 
-def _epoll_ctl(channel: Channel, loop, operation: int | Any):
-    # get epoll instance data
-    data: EpollData = loop.dispatcher_data
-    events = 0
+    def _epoll_ctl(self, operation: int):
+        events = 0
+        if self._channel.events & constants.CHANNEL_READ_EVENT:
+            events |= EPOLLIN
+        if self._channel.events & constants.CHANNEL_WRITE_EVENT:
+            events |= EPOLLOUT
 
-    if channel.events & constants.CHANNEL_READ_EVENT:
-        events |= EPOLLIN
-    if channel.events & constants.CHANNEL_WRITE_EVENT:
-        events |= EPOLLOUT
+        if operation == constants.EPOLL_CTL_ADD:
+            self.ep.register(fd=self._channel.fd, eventmask=events)
+        elif operation == constants.EPOLL_CTL_MOD:
+            self.ep.modify(fd=self._channel.fd, eventmask=events)
+        elif operation == constants.EPOLL_CTL_DEL:
+            self.ep.unregister(self._channel.fd)
+        else:
+            raise Exception('_epoll_ctl() operation is invalid.')
 
-    if operation == constants.EPOLL_CTL_ADD:
-        data.ep.register(fd=channel.fd, eventmask=events)
-    elif operation == constants.EPOLL_CTL_MOD:
-        data.ep.modify(fd=channel.fd, eventmask=events)
-    elif operation == constants.EPOLL_CTL_DEL:
-        data.ep.unregister(channel.fd)
-    else:
-        raise Exception('_epoll_ctl() operation is invalid.')
+    def add(self):
+        self._epoll_ctl(constants.EPOLL_CTL_ADD)
 
+    def modify(self):
+        self._epoll_ctl(constants.EPOLL_CTL_MOD)
 
-class EpollDispatcher(AbstractDispatcher):
-    """a handler that resolve dispatch problems"""
+    def remove(self):
+        self._epoll_ctl(constants.EPOLL_CTL_DEL)
+        self._channel.destroy_callback(self._channel.args)
 
-    def clear(self, loop):
-        data: EpollData = loop.dispatcher_data
-        close(data.efd)
-        del data
-
-    def add(self, channel: Channel, loop):
-        _epoll_ctl(channel, loop, constants.EPOLL_CTL_ADD)
-
-    def modify(self, channel: Channel, loop):
-        _epoll_ctl(channel, loop, constants.EPOLL_CTL_MOD)
-
-    def remove(self, channel: Channel, loop):
-        _epoll_ctl(channel, loop, constants.EPOLL_CTL_DEL)
-        channel.destroy_callback(channel.args)
-
-    def dispatch(self, loop, timeout: float):
-        data: EpollData = loop.dispatcher_data
-        # events = data.ep.poll()
-        data.events = data.ep.poll(timeout)
-        for index, fd, event_mask in enumerate(data.events):
+    def dispatch(self):
+        self.events = self.ep.poll(self._timeout)
+        for index, fd, event_mask in enumerate(self.events):
             if event_mask & EPOLLERR or event_mask & EPOLLHUP:
                 continue
             if event_mask & EPOLLIN:
-                event_active(loop, fd, constants.CHANNEL_READ_EVENT)
+                self._loop.event_active(fd, constants.CHANNEL_READ_EVENT)
             if event_mask & EPOLLOUT:
-                event_active(loop, fd, constants.CHANNEL_WRITE_EVENT)
+                self._loop.event_active(fd, constants.CHANNEL_WRITE_EVENT)
 
-
-def event_active(loop, fd, event):
-    # will implement in eventloop
-    pass
+    def set_channel(self, channel: Channel):
+        """
+        set dispatcher's channel instance,
+        this method will only call in eventloop,
+        similar to Future() in asyncio.eventloop.
+        """
+        self._channel = channel
