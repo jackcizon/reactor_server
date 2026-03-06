b1478eb64364129ce28424482cc3ef8740c9deab
jackcizon <jack20021213cn@gmail.com>
2026-03-06 15:59:33 +0800

    feat: server cls

---
+++ http_server/server.py
@@ -0,0 +1,46 @@
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+
+from http_server.channel import Channel
+from http_server.connection import Connection
+from http_server.constants import CHANNEL_READ_EVENT, EVENTLOOP_ACTION_ADD_CHANNEL
+from http_server.eventloop import EventLoop
+from http_server.thread.thread_pool import ThreadPool
+
+
+class Server:
+    def __init__(self, loop_cls: type[EventLoop], thread_num: int, host: str = 'localhost', port: int = 8000):
+        self._loop: EventLoop | None = loop_cls()
+        self._thread_num = thread_num
+        self._host = host
+        self._port = port
+        self._thread_pool = ThreadPool(self._loop, thread_num)
+        self._sock: socket | None = None
+        self._lfd: int | None = None
+
+        self._setup_sock()
+
+    def run(self):
+        self._thread_pool.run()
+        channel = Channel(
+            sock=self._sock,
+            event_mask=CHANNEL_READ_EVENT,
+            read_callback=self._accept_connection,
+            write_callback=None,
+            destroy_callback=None,
+            args=self
+        )
+        self._loop.add_task(channel=channel, action=EVENTLOOP_ACTION_ADD_CHANNEL)
+        self._loop.run()
+
+    def _setup_sock(self):
+        self._sock = socket(AF_INET, SOCK_STREAM, 0)
+        self._sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self._sock.setblocking(False)
+        self._sock.bind((self._host, self._port))
+        self._sock.listen(128)
+        self._lfd = self._sock.fileno()
+
+    def _accept_connection(self, args):
+        conn, addr = self._sock.accept()
+        loop = self._thread_pool.take_worker()
+        Connection(conn, loop)
