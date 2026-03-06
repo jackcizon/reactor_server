a63243ff464bde41da15147dbfee038f83e56ad9
jackcizon <jack20021213cn@gmail.com>
2026-03-06 15:19:21 +0800

    feat(thread): thread worker and thread pool

---
+++ http_server/thread/thread_pool.py
@@ -0,0 +1,40 @@
+import inspect
+import threading
+
+from http_server.eventloop import EventLoop
+from http_server.thread.worker_thread import WorkerThread
+
+
+class ThreadPool:
+    def __init__(self, loop: EventLoop, thread_num: int):
+        self._loop = loop
+        self._thread_num = thread_num
+        self._is_start = False
+        self._workers: list[WorkerThread] = []
+        self._index = 0
+
+    def run(self):
+        self._check_runtime()
+        self._is_start = True
+        if self._thread_num > 0:
+            for index in range(self._thread_num):
+                sub_thread = WorkerThread(index_in_pool=index)
+                sub_thread.run()
+                self._workers.append(sub_thread)
+
+    def _take_worker(self):
+        self._check_runtime()
+        loop = self._loop
+        if self._thread_num > 0:
+            loop = self._workers[self._index].loop
+            self._index += 1
+            self._index %= self._thread_num
+        return loop
+
+    def _check_runtime(self):
+        if self._is_start:
+            frame = inspect.stack()[0]
+            raise RuntimeError(f'ThreadPool has already started, line: {frame.lineno}, func: {frame.function}')
+        if self._loop.thread_id != threading.get_ident():
+            frame = inspect.stack()[0]
+            raise RuntimeError(f'loop id error, line: {frame.lineno}, func: {frame.function}')
---
+++ http_server/thread/worker_thread.py
@@ -0,0 +1,34 @@
+import threading
+
+from http_server.eventloop import EventLoop
+
+
+class WorkerThread:
+    def __init__(self, index_in_pool: int | None = None):
+        self._index_in_pool = index_in_pool
+        self._name = f'SubThread-{index_in_pool}'
+        self._thread: threading.Thread | None = None
+        self._thread_id: int | None = None
+        self._mutex = threading.RLock()
+        self._condition = threading.Condition()
+        self._loop: EventLoop | None = None
+
+    def run(self):
+        self._thread = threading.Thread(target=self._target)
+        self._thread.start()
+
+        with self._condition:
+            while self._loop is None:
+                self._condition.wait()
+
+    def _target(self):
+        self._thread_id = threading.get_ident()
+
+        with self._condition:
+            self._loop = EventLoop(thread_name=self._name)
+            self._condition.notify()
+        self._loop.run()
+
+    @property
+    def loop(self):
+        return self._loop
