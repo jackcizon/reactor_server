7c7729be05db5b842f8af32862224732575aa50d
jackcizon <jack20021213cn@gmail.com>
2026-03-09 14:42:52 +0800

    fix: thread_pool check runtime fixup.

--- http_server/thread/thread_pool.py
+++ http_server/thread/thread_pool.py
@@ -23,7 +23,7 @@ class ThreadPool:
                 self._workers.append(sub_thread)
 
     def take_worker(self):
-        self._check_runtime()
+        # self._check_runtime()
         loop = self._loop
         if self._thread_num > 0:
             loop = self._workers[self._index].loop
@@ -35,6 +35,7 @@ class ThreadPool:
         if self._is_start:
             frame = inspect.stack()[0]
             raise RuntimeError(f'ThreadPool has already started, line: {frame.lineno}, func: {frame.function}')
-        if self._loop.thread_id != threading.get_ident():
+        # if self._loop.thread_id != threading.get_ident():
+        if self._loop.thread_id is not None and self._loop.thread_id != threading.get_ident():
             frame = inspect.stack()[0]
             raise RuntimeError(f'loop id error, line: {frame.lineno}, func: {frame.function}')
