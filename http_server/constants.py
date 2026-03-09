14757caa20e7a84143f10fca5094875baa3e8634
jackcizon <jack20021213cn@gmail.com>
2026-03-09 18:15:01 +0800

    feat: add more http status code related consts.

--- http_server/constants.py
+++ http_server/constants.py
@@ -16,3 +16,19 @@ HTTP_PARSE_REQUEST_LINE: Final[int] = 1
 HTTP_PARSE_REQUEST_HEADERS: Final[int] = 2
 HTTP_PARSE_REQUEST_BODY: Final[int] = 3
 HTTP_PARSE_REQUEST_DONE: Final[int] = 4
+
+STATUS_UNKNOWN: Final[int] = 0
+STATUS_OK: Final[int] = 200
+STATUS_MOVE_PERM: Final[int] = 301
+STATUS_MOVE_TEMP: Final[int] = 302
+STATUS_BAD_REQUEST: Final[int] = 400
+STATUS_NOT_FOUND: Final[int] = 404
+STATUS_SERVER_ERROR: Final[int] = 500
+
+HTTP_STATUS_CODES_MSG_MAP = {
+    200: "OK",
+    301: "Moved Permanently",
+    302: "Found",
+    400: "Bad Request",
+    404: "Not Found",
+}
