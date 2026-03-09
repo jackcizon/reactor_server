d41c5b9f4ebe9d492d92a69b92aa4e42af2a1716
jackcizon <jack20021213cn@gmail.com>
2026-03-09 14:40:15 +0800

    feat: base connection cls

--- http_server/connection.py
+++ http_server/connection.py
@@ -2,16 +2,19 @@ from socket import socket
 
 from http_server.buffer import Buffer
 from http_server.channel import Channel
-from http_server.constants import CHANNEL_READ_EVENT, EVENTLOOP_ACTION_ADD_CHANNEL
+from http_server.constants import CHANNEL_READ_EVENT, EVENTLOOP_ACTION_ADD_CHANNEL, EVENTLOOP_ACTION_DELETE_CHANNEL, \
+    EVENTLOOP_ACTION_MODIFY_CHANNEL
 from http_server.eventloop import EventLoop
+from http_server.http.request import Request
+from http_server.http.response import Response
 
 
 class Connection:
-    def __init__(self, sock: socket, loop: EventLoop, name: str = None):
+    def __init__(self, sock: socket, loop: EventLoop):
         self._sock = sock
         self._fd = sock.fileno()
         self._loop = loop
-        self._name = name
+        self._name = f'connection-{self._fd}'
         self._channel = Channel(
             sock=self._sock,
             event_mask=CHANNEL_READ_EVENT,
@@ -22,21 +25,32 @@ class Connection:
         )
         self._read_buf = Buffer(10240)
         self._write_buf = Buffer(10240)
-        self._request = None
-        self._response = None
+        self._request = Request()
+        self._response = Response()
         self._loop.add_task(channel=self._channel, action=EVENTLOOP_ACTION_ADD_CHANNEL)
 
     def read(self):
         count = self._read_buf.socket_read(sock=self._sock)
         if count > 0:
             # get new http request
-            pass
+            print('\n--------------------------------')
+            print(self._read_buf.data)
+            print('--------------------------------\n')
+            flag = self._request.parse_http_request(self._sock, self._read_buf, self._write_buf, self._response)
+            if flag is None:
+                self._write_buf.append('HTTP/1.1 400 Bad Request\r\n\r\n')
         else:
-            # disconnect
-            pass
+            self._loop.add_task(self._channel, EVENTLOOP_ACTION_DELETE_CHANNEL)
+            print('connection lost')
 
     def write(self):
-        pass
+        count = self._write_buf.send_data(self._channel.sock)
+        if count > 0:
+            if self._write_buf.readable_size > 0:
+                self._channel.writable(False)
+                self._loop.add_task(self._channel, EVENTLOOP_ACTION_MODIFY_CHANNEL)
+                self._loop.add_task(self._channel, EVENTLOOP_ACTION_DELETE_CHANNEL)
 
     def destroy(self):
-        pass
+        if self is not None:
+            del self
