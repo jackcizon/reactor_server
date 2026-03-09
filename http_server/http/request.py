764e00fd2c897c0eff8cd8fc7a10445d4fabe3f9
jackcizon <jack20021213cn@gmail.com>
2026-03-09 18:17:14 +0800

    feat: handle http get request for file and dir.

--- http_server/http/request.py
+++ http_server/http/request.py
@@ -1,14 +1,9 @@
 import os
-from pathlib import Path
-from socket import socket
 
 from http_server.buffer import Buffer
 from http_server.constants import HTTP_PARSE_REQUEST_LINE, HTTP_PARSE_REQUEST_DONE, HTTP_PARSE_REQUEST_HEADERS, \
     HTTP_PARSE_REQUEST_BODY
-from http_server.utils import get_file_type
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+from http_server.http.response import Response
 
 
 def _debug_process_http_request():
@@ -37,11 +32,11 @@ def _debug_parse_body():
 
 class Request:
     def __init__(self):
-        self._path = None
-        self._method = None
-        self._version = None
-        self._state = HTTP_PARSE_REQUEST_LINE
-        self._headers = {}
+        self._path: str | None = None
+        self._method: str | None = None
+        self._version: str | None = None
+        self._state: int = HTTP_PARSE_REQUEST_LINE
+        self._headers: dict[str, str] = {}
 
     def add_header(self, key, value):
         self._headers[key] = value
@@ -65,6 +60,10 @@ class Request:
     def set_state(self, state):
         self._state = state
 
+    @property
+    def method(self):
+        return self._method
+
     def parse_http_request(self, sock, read_buf, write_buf, response):
         while True:
             if self._state == HTTP_PARSE_REQUEST_LINE:
@@ -78,6 +77,7 @@ class Request:
                 # exit()  # debug usage
             elif self._state == HTTP_PARSE_REQUEST_DONE:
                 # _debug_process_http_request()
+                self._process_http_request(write_buf, response, sock)
                 # response.prepare_message(write_buf, sock)
                 break
             else:
@@ -103,3 +103,27 @@ class Request:
         """body is empty, because server only supports [GET]"""
         # _debug_parse_body()
         self.set_state(HTTP_PARSE_REQUEST_DONE)
+
+    def _process_http_request(self, write_buf, response: Response, sock):
+        """only allow HTTP GET"""
+        # allow GET
+        if self.method.upper() != 'GET':
+            return None
+
+        # is root path
+        if self._path == '/':
+            file = './'
+        else:
+            file = self._path.lstrip('/')  # e.g: /robots.txt, rm '/', get 'robots.txt'
+
+        # file not exists
+        try:
+            os.stat(file)
+        except FileNotFoundError:
+            response.send_404_html(write_buf, sock)
+
+        if os.path.isdir(file):
+            response.send_dir(dirname=file, write_buf=write_buf, sock=sock)
+
+        if os.path.isfile(file):
+            response.send_file(file, write_buf, sock)
