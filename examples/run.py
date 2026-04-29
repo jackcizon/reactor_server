21bd34cc8325d9e17029d7d52d58b2ef82e944b8
jackcizon <jack20021213cn@gmail.com>
2026-04-30 03:57:27 +0800

    feat: enable any doc root.

--- examples/run.py
+++ examples/run.py
@@ -2,4 +2,4 @@ from reactor_server.http_server.eventloop import EventLoop
 from reactor_server.http_server.server import Server
 
 if __name__ == '__main__':
-    Server(loop_cls=EventLoop, root="/home/jack/code/python/learn/reactor_server/tests").run()
+    Server(loop_cls=EventLoop, root="/home/jack/code/python/learn/reactor_server/reactor_server").run()
